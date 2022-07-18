6bbb2fb15711b1961f26d3b5823134f98984fff8
atif.masood <atif.masood@accrualhub.com>
2022-07-18 12:43:41 +0500

    fix error of blog page

--- app/Http/Controllers/brand/BlogController.php
+++ app/Http/Controllers/brand/BlogController.php
@@ -174,7 +174,7 @@ class BlogController extends Controller
                 
                 @unlink(public_path()."/blog/".$filePath->image );
                
-                Redirect::back();
+                return Redirect::back();
         // } catch (\Exception $exception) {
         //     return Redirect::back();
         // }
