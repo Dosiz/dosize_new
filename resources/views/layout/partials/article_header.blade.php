21dc54b0f2d4527d500165964ccfd2b98b38a4f6
atif.masood <atif.masood@accrualhub.com>
2022-07-27 10:48:34 +0500

    article like and bookmark done 4

--- resources/views/layout/partials/article_header.blade.php
+++ resources/views/layout/partials/article_header.blade.php
@@ -39,9 +39,9 @@
                             <ul>
                                 <li>
                                     @if(count($blog_comments) > 0)
-                                    <span class="blog_comment_count"> {{count($blog_comments)}} </span>
+                                    <span class="comment_count"> {{count($blog_comments)}} </span>
                                     @else
-                                    <span class="blog_comment_count"> 0 </span>
+                                    <span class="comment_count"> 0 </span>
                                     @endif
                                     <img src="{{ asset('assets/img/mobile_component/notificationIcon.png') }}" alt=""
                                         class="img-fluid desktop_hide">
