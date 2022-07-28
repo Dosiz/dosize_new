1affd4b8997b1563abf49df0aa3d9b639c3b02f4
atif.masood <atif.masood@accrualhub.com>
2022-07-28 10:36:58 +0500

    blog likes revert on landing page 2

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -344,7 +344,7 @@ Dosize
                                                     class="img-fluid">
                                             </h3>
                                             <span class="read_more">
-                                                <a href="{{route('category_by_city',['category_id'=>$category->id,'city_id'=>5])}}" class="font-size-12 font-weight-400">
+                                                <a href="{{route('category_by_city',['category_id'=>$product_category->id,'city_id'=>5])}}" class="font-size-12 font-weight-400">
                                                     כתבות ביגוד והנעלה</a> </span>
                                         </div>
                                     </div>
