022c4b701403b905c7e0a2bf5a9dcb263eff21fa
atif.masood <atif.masood@accrualhub.com>
2022-08-19 14:07:06 +0500

    status and order page done 3

--- resources/views/frontend/city_category.blade.php
+++ resources/views/frontend/city_category.blade.php
@@ -130,7 +130,7 @@ Category By city
                                         {{$blog->title}}
                                     </h4>
                                     <p class="discription font-size-10 font-weight-400">
-                                        {!! substr($blog->description, 0,  30) !!}
+                                        {!! substr($blog->description, 0,  50) !!}
                                     </p>
                                     <span class="font-size-12">{{$blog->totallikes}} <i class="fa fa-heart"
                                             aria-hidden="true"></i></span>
