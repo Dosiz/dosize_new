43494f8491b2d13995e0a7364498e745f670aa56
atif.masood <atif.masood@accrualhub.com>
2022-09-01 12:26:09 +0500

    final css issue 7

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -522,7 +522,7 @@ Dosize
                                                                 {{\Illuminate\Support\Str::limit($blog->title ?? '',30,'...')}}
                                                             </h4>
                                                             <p class="discription font-size-12 font-weight-400">
-                                                                {!! html_entity_decode(\Illuminate\Support\Str::limit($blog->description ?? '',60,'...')) !!}
+                                                                {!! json_decode(\Illuminate\Support\Str::limit($blog->description ?? '',60,'...')) !!}
                                                             </p>
                                                             
                                                         </div>
