08caa200a815102d2687daa864b0976c35fc96b5
atif.masood <atif.masood@accrualhub.com>
2022-09-02 15:04:14 +0500

    front end fixes 26

--- resources/views/frontend/city_category.blade.php
+++ resources/views/frontend/city_category.blade.php
@@ -123,7 +123,7 @@ Category By city
                 </div>
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="affordable_consumption_list d-flex multiple_afforable_consumption">
+                        <div class="affordable_consumption_list d-flex multiple_afforable_consumption justify-content-between">
                             @if(count($blogs) > 0)
                             @foreach($blogs as $blog)
                             <div class="affordable_consumption_box box_shahdow">
