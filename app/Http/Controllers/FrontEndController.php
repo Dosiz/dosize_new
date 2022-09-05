29cab0b2e2f6e178081bcbc2c47cfeac8d750445
atif.masood <atif.masood@accrualhub.com>
2022-09-05 11:46:22 +0500

    blogs on landing page 1

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -90,7 +90,7 @@ class FrontEndController extends Controller
         )
         ->where('blogs_has_cities.city_id',$city_id)
         ->where('blogs.status',1)
-        ->take(4)
+        ->take(3)
         ->orderBy('id', 'DESC')
         // ->where('categories.id',$category_id)
         ->get();
