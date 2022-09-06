1aa1f5700b260ea57a81ae011b7b4fb030c3e0f3
atif.masood <atif.masood@accrualhub.com>
2022-09-06 17:26:39 +0500

    alt change 11

--- resources/views/layout/partials/brand_signup_footer.blade.php
+++ resources/views/layout/partials/brand_signup_footer.blade.php
@@ -12,15 +12,7 @@
 				</div>
 				<div class="col-md-4 text-center pt-4">
 					<p style="font-size: 18px; color: #fff !important; ">
-						{{-- <a href="https://api.whatsapp.com/send?phone={{$brand_profile->whatsapp_no ?? ''}}&amp;text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." style="font-size: 18px; color: #fff !important; "> {{$brand_profile->whatsapp_no ?? ''}} <i class="fa fa-phone"></i></a> --}}
-						<?php
-						if(str_split($brand_profile->whatsapp_no)[0]=="+" || str_split($brand_profile->whatsapp_no)[0]=="9"){
-						print '<a href="https://api.whatsapp.com/send?phone='.$brand_profile->whatsapp_no ?? ''.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">';
-						}
-						else{
-						print '<a href="https://api.whatsapp.com/send?phone=+972'.$brand_profile->whatsapp_no ?? ''.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">';
-						}
-						?>
+						<a href="https://api.whatsapp.com/send?phone={{$brand_profile->whatsapp_no ?? ''}}&amp;text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." style="font-size: 18px; color: #fff !important; "> {{$brand_profile->whatsapp_no ?? ''}} <i class="fa fa-phone"></i></a>
 					</p>
 				</div>
 				<div class="col-md-4 text-center pt-4">
@@ -30,14 +22,7 @@
 				</div>
 			</div>
 		</div>
-		<?php
-				if(str_split($brand_profile->whatsapp_no)[0]=="+" || str_split($brand_profile->whatsapp_no)[0]=="9"){
-				  print '<a href="https://api.whatsapp.com/send?phone='.$brand_profile->whatsapp_no ?? ''.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">';
-				}
-				else{
-				  print '<a href="https://api.whatsapp.com/send?phone=+972'.$brand_profile->whatsapp_no ?? ''.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">';
-				}
-				?>			  
+		<a href="https://api.whatsapp.com/send?phone={{$brand_profile->whatsapp_no ?? ''}}&amp;text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">			  
 			<i class="fab fa-whatsapp my-float" aria-hidden="true"></i>
 			<div id="float_text">זמינים עבורכם כאן!</div>
 			</a>
