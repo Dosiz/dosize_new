598c4b65ed6802522d2279595ef194b7583d2844
atif.masood <atif.masood@accrualhub.com>
2022-09-06 17:33:00 +0500

    alt change 13

--- resources/views/layout/partials/brand_signup_footer.blade.php
+++ resources/views/layout/partials/brand_signup_footer.blade.php
@@ -27,7 +27,7 @@
 			<a href="https://api.whatsapp.com/send?phone='.{{$brand_profile->whatsapp_no}}.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">
 
 			@else
-			<a href="https://api.whatsapp.com/send?phone=+972'.{{$brand_profile->whatsapp_no}}.'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">
+			<a href="https://api.whatsapp.com/send?phone=+972'{{$brand_profile->whatsapp_no}}'&text=%D7%A9%D7%9C%D7%95%D7%9D%20%D7%95%D7%91%D7%A8%D7%9B%D7%94%2C%20%D7%90%D7%A0%D7%99%20%D7%A4%D7%95%D7%A0%D7%94%20%D7%90%D7%9C%D7%99%D7%9A%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%91%D7%93%D7%95%D7%A1%D7%99%D7%96%20%D7%A6%D7%A8%D7%9B%D7%A0%D7%95%D7%AA%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%91%D7%91%D7%A7%D7%A9%D7%94..." class="float" target="_blank">
 			@endif
 
 			<i class="fab fa-whatsapp my-float" aria-hidden="true"></i>
