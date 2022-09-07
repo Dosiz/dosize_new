04e768a4c0b891ab954dd0a41107d824818b92ac
atif.masood <atif.masood@accrualhub.com>
2022-09-07 15:47:07 +0500

    city and profile id has been fixed 1

--- resources/views/layout/article.blade.php
+++ resources/views/layout/article.blade.php
@@ -10,7 +10,7 @@
     <meta property="og:locale" content="he_IL" />
     <meta property="og:type" content="article" />
     <meta property="og:site_name" content="דוסיז צרכנות" />
-    <meta property="og:url" content="http://arikliger.com/article/{{$blog->id ?? ''}}/%d7%91%d7%99%d7%aa-%d7%94%d7%90%d7%95%d7%a4%d7%a0%d7%94-golbary-%d7%9e%d7%a9%d7%99%d7%a7-%d7%a7%d7%95%d7%9c%d7%a7%d7%a6%d7%99%d7%99%d7%aa-%d7%91%d7%99%d7%a9%d7%95%d7%9d-%d7%9c%d7%a0%d7%a9%d7%99%d7%9d/" />
+    <meta property="og:url" content="http://{{current(explode('.', request()->getHost()))}}.arikliger.com/article/{{$blog->id ?? ''}}/%d7%91%d7%99%d7%aa-%d7%94%d7%90%d7%95%d7%a4%d7%a0%d7%94-golbary-%d7%9e%d7%a9%d7%99%d7%a7-%d7%a7%d7%95%d7%9c%d7%a7%d7%a6%d7%99%d7%99%d7%aa-%d7%91%d7%99%d7%a9%d7%95%d7%9d-%d7%9c%d7%a0%d7%a9%d7%99%d7%9d/" />
     <meta property="og:image:alt" content="{{$blog->title ?? ''}}" />
     <meta name="twitter:title" content="{{$blog->title ?? ''}}" />
     <meta property="og:image" content="{{asset('blog/'.$blog->image ?? '')}}" />
