0ada5323cc62ecabfba51d9a8ee0c369378ebec1
atif.masood <atif.masood@accrualhub.com>
2022-09-08 16:37:13 +0500

    static website 1

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
+Route::get('/static', function () {
     return view('index');
 });
 
