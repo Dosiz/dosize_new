9975d06f03a92bf6839735c0bf19d9926e73ba89
atif.masood <atif.masood@accrualhub.com>
2022-09-08 10:44:34 +0500

    mobile view logoutand sigup working

--- resources/views/layout/partials/mobile_side_menu.blade.php
+++ resources/views/layout/partials/mobile_side_menu.blade.php
@@ -1,8 +1,16 @@
 <div class="mobile_side_menu">
     <h4>צהריים טובים!</h4>
     <div class="auth_button">
-        <a class="enrollemnt_button" data-toggle="modal" data-target="#enrollmentModal2">הרשמה</a>
-        <a class="" data-toggle="modal" data-target="#enrollmentModal">התחברות</a>
+        @if(! isset(Auth::user()->name))
+            <a class="enrollemnt_button" data-toggle="modal" data-target="#enrollmentModal2">הרשמה</a>
+            <a href="" data-toggle="modal" data-target="#enrollmentModal">התחברות</a>
+        @else
+            @if(Auth::user()->hasRole('User'))
+                <p style="color:aliceblue"> ברוך הבא {{Auth::user()->name }} </p>
+            @else
+            <a class="enrollemnt_button" href="{{route('dashboard')}}"> לוּחַ מַחווָנִים </a>
+            @endif
+        @endif
     </div>
     <div class="mobile_menu_list">
         <ul>
@@ -11,8 +19,11 @@
                         class="img-fluid"></a>
             </li>
             <li>
-                <a href="{{route('user.wallet')}}">כתבות צרכנות <img src="{{asset('assets/img/mobile_component/cartIcon.png') }}" alt=""
-                        class="img-fluid"></a>
+                @guest
+                    <a href="" data-toggle="modal" data-target="#enrollmentModal2">כתבות צרכנות <img src="{{asset('assets/img/mobile_component/cartIcon.png') }}" alt="" class="img-fluid"></a>
+                @else
+                    <a href="{{route('user.wallet')}}">כתבות צרכנות <img src="{{asset('assets/img/mobile_component/cartIcon.png') }}" alt="" class="img-fluid"></a>
+                @endguest
             </li>
             <li>
                 
@@ -26,14 +37,17 @@
                 <a href="{{route('archive_cat')}}">התראות <img src="{{asset('assets/img/mobile_component/bellIcon.png') }}" alt=""
                         class="img-fluid"></a>
             </li>
+            @guest
+            @else
             <li>
-                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><img src="{{asset('assets/img/mobile_component/logout_icon.png') }}" alt=""
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">התנתקות <img src="{{asset('assets/img/mobile_component/logoutIcon.png') }}" alt=""
                     class="img-fluid"></a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
                 @csrf
             </form>
             </li>
+            @endguest
         </ul>
     </div>
 </div>
