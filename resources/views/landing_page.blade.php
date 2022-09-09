7c493e000684541f2d9f184bc08ddd29d3d969ca
atif.masood <atif.masood@accrualhub.com>
2022-09-09 17:33:08 +0500

    category on city page 3

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -359,7 +359,7 @@ Dosize
         <?php $i = 1; ?>
         <?php $a =1; ?>
         @if ($p_city )
-        @if ( count($p_city->blogs->groupBy('category_id')) > 0 )
+        @if (count($p_city->products->groupBy('category_id')) > 0 || count($p_city->blogs->groupBy('category_id')) > 0 )
             <div class="row flex-row-reverse" style="min-height:600px; position: relative; display:flex;">
             @foreach ($p_city->products->groupBy('category_id') as $key=>$product_categories)
             
