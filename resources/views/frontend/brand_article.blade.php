76ccc1d7cd8f919523ea6aeebd96c3fe01c8335b
atif.masood <atif.masood@accrualhub.com>
2022-09-12 13:13:55 +0500

    point 8 has been done 19

--- resources/views/frontend/brand_article.blade.php
+++ resources/views/frontend/brand_article.blade.php
@@ -410,7 +410,7 @@
                             @foreach($recomanded_blogs as $recomanded_blog)
                             <div class="affordable_consumption_box box_shahdow">
                                 <a class="font-size-14 font-weight-700" href="{{route('article',$recomanded_blog->recomended_blog->id ?? '')}}">
-                                    <img src="{{asset('blog/'.$recomanded_blog->recomended_blog->image)}}" alt="" class="img-fluid" style="width:131px; height:181px;">
+                                    <img src="{{asset('blog/'.$recomanded_blog->recomended_blog->image)}}" alt="" class="img-flui" style="width:131px; height:181px;">
                                 </a>
                                 <div class="content_div">
                                     <span class="category font-size-12 font-weight-400">{{\Illuminate\Support\Str::limit($blog->brandprofile->brand_name?? '',15)}} </span>
@@ -419,7 +419,8 @@
                                         {{$recomanded_blog->recomended_blog->title}}
                                     </h4>
                                     <p class="discription font-size-10 font-weight-400">
-                                        {!! $recomanded_blog->recomended_blog->description ?? '' !!}
+                                        {{-- {!! $recomanded_blog->recomended_blog->description ?? '' !!} --}}
+                                        {{ \Illuminate\Support\Str::limit(strip_tags($recomanded_blog->recomended_blog->description) ?? '',60,'...') }}
                                     </p>
                                     </a>
                                     {{-- <span class="font-size-12">4 <i class="fa fa-heart"
