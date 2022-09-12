46136cd906e84d0aefca20a5d4dbd91cb8ae2c7d
atif.masood <atif.masood@accrualhub.com>
2022-09-12 16:00:30 +0500

    point 2 again done 3

--- resources/views/frontend/article.blade.php
+++ resources/views/frontend/article.blade.php
@@ -411,7 +411,7 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="affordable_consumption_list d-flex multiple_afforable_consumption justify-content-between">
+                        <div class="affordable_consumption_list d-flex multiple_afforable_consumption justify-content-end">
                             @if(count($recomanded_blogs) > 0)
                             @foreach($recomanded_blogs as $recomanded_blog)
                             <div class="affordable_consumption_box box_shahdow">
