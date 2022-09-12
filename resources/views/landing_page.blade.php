0bb598c82ab5dbe07f0268bd9cb36738d80f446a
atif.masood <atif.masood@accrualhub.com>
2022-09-12 10:27:35 +0500

    point 24 working 1

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -547,7 +547,7 @@ Dosize
                                                     @endif
                                                 @endforeach
 
-                                                @foreach($product_categories->take(2)->latest() as $product)
+                                                @foreach($product_categories->take(2)->orderBy('id', 'DESC') $product)
                                                 <div class="affordable_consumption_box box_shahdow">
                                                     <a class="font-size-14 font-weight-700" href="{{route('product',$product->id ?? '')}}">
                                                         <img src="{{asset('product/'.$product->image ?? '')}}" class="imgresponsive" alt="" width="131px" height="100%">
