993645ee23d348fd50484d124d7c30002c6ee3f9
atif.masood <atif.masood@accrualhub.com>
2022-09-19 11:00:10 +0500

    point 15 done 10

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -136,7 +136,12 @@
                     <div class="row">
                         <div class="col-6 col-xl-12 text-left d-xl-none">
                             <div class="product_price">
-                                <p>{{$product->price}} ₪ @if($product->discout_price)<span class="font-size-14">{{$product->discout_price}} ₪</span>@endif</p>
+                                @if($product->discout_price)
+                                <p>{{$product->discount_price}} ₪ <span class="font-size-14">{{$product->price}} ₪</span></p>
+                                @else
+                                <p>{{$product->price}} ₪ </p>
+                                @endif
+
                             </div>
                         </div>
                         <div class="col-6 col-xl-12 text-right">
