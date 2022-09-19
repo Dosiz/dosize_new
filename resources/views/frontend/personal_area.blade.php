956ded9ebc6a80b74931cdb02a2ec57a2797365c
atif.masood <atif.masood@accrualhub.com>
2022-09-19 12:35:22 +0500

    personal area 5

--- resources/views/frontend/personal_area.blade.php
+++ resources/views/frontend/personal_area.blade.php
@@ -43,6 +43,73 @@ Dosize
             </div>
         </div>
         <div class="line spacing"></div>
+        <div class="hot_flashes_div spacing">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <form action="" method="POST">
+                            @csrf
+                            <div class="inputDiv">
+                                <label for="" class="font-size-16">שם</label>
+                                <input id="name" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <span class="text-danger name_valid">{{$errors->first('name')}}</span>
+                            </div>
+                            <div class="inputDiv">
+                                <label for="" class="font-size-16">עיר</label>
+                                <select name="city_id" id="city_id">
+                                    <option selected disabled value="">בחר מתוך הרשימה</option>
+                                    @foreach($cities as $city)
+                                        <option value="{{$city->id}}"> {{$city->hebrew_name}} </option>
+                                    @endforeach
+                                </select>
+                                <span class="text-danger city_valid">{{$errors->first('city_id')}}</span>
+                            </div>
+                            <div class="inputDiv">
+                                <label for="" class="font-size-16">דוא”ל</label>
+                                <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <span class="text-danger email_valid">{{$errors->first('email')}}</span>
+                            </div>
+                            <div class="inputDiv">
+                                <label for="" class="font-size-16">סיסמה</label>
+                                <div class="password_div">
+                                    <input id="password" type="password" name="password" required autocomplete="new-password">
+                                    <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                    <span class="text-danger password_valid">{{$errors->first('password')}}</span>
+                                </div>
+                            </div>
+                            <div class="checkBox_div">
+                                <input type="checkbox" name="" id="approve" checked>
+                                <label for="approve" class="font-size-16">אני מאשר קבלת תכנים מדוסיז צרכנות.</label>
+                            </div>
+                            <div class="checkBox_div">
+                                <input type="checkbox" name="" id="policy" checked>
+                                <label for="policy" class="font-size-16">אני מסכים <a href="">למדיניות</a>
+                                    המערכת...</label>
+                            </div>
+                            <button type="submit" class="font-size-16" style="cursor: pointer;">הרשמה</button>
+                            <div class="sign_up_with">
+                                <h6 class="text-center">או הרשם עם</h6>
+                                <div class="signup_btn">
+                                    <a href="{{route('auth.facebook')}}">
+                                        <img src="{{ asset('assets/img/mobile_component/facebookIcon.png') }}" alt=""
+                                            class="img-fluid">
+                                    </a>
+                                    <a href="{{route('auth.google')}}">
+                                        <img src="{{ asset('assets/img/mobile_component/googleIcon.png') }}" alt=""
+                                            class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- <div class="d-flex justify-content-center mt-4">
+                                <a href="" id="signup_btn" class="text-dark">
+                                    <b>אין לכם חשבון? לחצו כאן להרשמה > </b>
+                                </a>
+                            </div> -->
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         
         <!-- main footer start from here -->
