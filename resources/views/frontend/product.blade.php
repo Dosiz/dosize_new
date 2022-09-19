74c2dda76d63fd987808c80ca35463f6a5d39278
atif.masood <atif.masood@accrualhub.com>
2022-09-19 10:32:09 +0500

    point 15 done

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -136,7 +136,7 @@
                     <div class="row">
                         <div class="col-6 col-xl-12 text-left d-xl-none">
                             <div class="product_price">
-                                <p>50 ₪ <span class="font-size-14">80 ₪</span></p>
+                                <p>{{$product->price}} ₪ <span class="font-size-14">80 ₪</span></p>
                             </div>
                         </div>
                         <div class="col-6 col-xl-12 text-right">
