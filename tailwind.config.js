2059aeb1458c589e062ba1adb23d8cdb463fb9b9
Yinon Rahamim <ynonra@gmail.com>
2022-09-28 13:36:31 +0300

    contact links UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/views/**/*.blade.php",
-    ],
+    content: ["./resources/views/**/*.blade.php"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                pink: "#DB1580",
+            },
+        },
     },
     plugins: [],
-    prefix: 'tw-',
+    prefix: "tw-",
 };
